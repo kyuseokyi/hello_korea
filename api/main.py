dc96f33970a0e5b1167e5b9f1e5b0e09a13e4182
yi kyuseok <kyuseok.y@gmail.com>
2024-06-30 23:41:40 +0900

    add router api question

--- api/main.py
+++ api/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
+from domain.question import question_router
 # import models
 #
 # from database import engine
@@ -7,6 +9,20 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/hello")
-def hello():
-    return {"Hello": "World"}
+origins = [
+    "http://127.0.0.1:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# @app.get("/hello")
+# def hello():
+#     return {"message": "World"}
+
+app.include_router(question_router.router)

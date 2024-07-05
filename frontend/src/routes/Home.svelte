df761e7d8db765c439e6068103873bdd4c39ac6c
yi kyuseok <kyuseok.y@gmail.com>
2024-07-06 01:24:28 +0900

    add detail page

--- frontend/src/routes/Home.svelte
+++ frontend/src/routes/Home.svelte
@@ -1,20 +1,17 @@
 <script>
+    import fastapi from "../lib/api"
+    import { link } from 'svelte-spa-router'
+
     let question_list = []
 
-    function get_question_list() {
-        fetch("http://127.0.0.1:8000/api/question/list").then((response) => {
-            response.json().then((json) => {
-                question_list = json
-            })
-        })
-    }
-
-    get_question_list()
+    fastapi('get', '/api/question/list', {}, (json) => {
+        question_list = json
+    })
 </script>
 
 <ul>
     {#each question_list as question}
-        <li>{question.subject}</li>
+        <li><a use:link href="/detail/{question.id}">{question.subject}</a></li>
     {/each}
 </ul>
-  
+                                                                    

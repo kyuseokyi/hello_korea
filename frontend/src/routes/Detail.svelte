0d9c3c4c5738099265a34d538934431497792910
yi kyuseok <kyuseok.y@gmail.com>
2024-07-10 23:43:14 +0900

    fronted answer

--- frontend/src/routes/Detail.svelte
+++ frontend/src/routes/Detail.svelte
@@ -1,25 +1,40 @@
 <script>
-    import fastapi from "../lib/api"
+  import fastapi from "../lib/api"
 
-    export let params = {}
-    let question_id = params.question_id
-    let question = {}
-    let content = ""
+  export let params = {}
+  let question_id = params.question_id
+  let question = {}
+  let content = ""
 
-    function get_question() {
-        fastapi("get", "/api/question/detail/" + question_id, {}, (json) => {
-            question = json
-        })
+  function get_question() {
+    fastapi("get", "/api/question/detail/" + question_id, {}, (json) => {
+      question = json
+    })
+  }
+
+  get_question()
+
+  function post_answer(event) {
+    console.log("?????")
+    event.preventDefault()
+    let url = "/api/answer/create/" + question_id
+    let params = {
+      content: content
     }
-
-    get_question()
+    fastapi('post', url, params,
+      (json) => {
+        content = ''
+        get_question()
+      }
+    )
+  }
 </script>
 
 <h1>{question.subject}</h1>
 <div>
-    {question.content}
+  {question.content}
 </div>
 <form method="post">
-    <textarea rows="15" bind:value={content}></textarea>
-    <input type="submit" value="답변등록">
+  <textarea rows="15" bind:value={content}></textarea>
+  <input type="submit" value="답변등록" on:click="{post_answer}">
 </form>
